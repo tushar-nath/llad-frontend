309ea2deaa15e29be94e964b892f905c3b042f43
bhardwajakshit <bhardwajakshit78@gmail.com>
2024-02-08 18:30:35 +0530

    Updated user profile picture placeholder

--- src/components/sidebar/sidebarProfile.tsx
+++ src/components/sidebar/sidebarProfile.tsx
@@ -1,19 +1,21 @@
 import { useContext } from "react";
 import { UserContext } from "../../contexts/userContext";
+import { UserPlaceholder } from "../../svgs/userPlaceholder";
 
 const SidebarProfile = () => {
   const { user } = useContext(UserContext);
   return (
     <div>
       <button>
-        <img
-          src={
-            user?.profilePicture ||
-            "https://media.licdn.com/dms/image/C4D03AQF2PvP5Mo9wjw/profile-displayphoto-shrink_800_800/0/1659554322605?e=1712188800&v=beta&t=QmQQari0XT7-2bCDzzHHfKHQumxxVzAQqNgXb8fujro"
-          }
-          alt="profile"
-          className="w-14 h-14 rounded-full"
-        />
+        {user?.profilePicture ? (
+          <img
+            src={user?.profilePicture}
+            alt="profile"
+            className="w-14 h-14 rounded-full"
+          />
+        ) : (
+          <UserPlaceholder />
+        )}
       </button>
     </div>
   );
