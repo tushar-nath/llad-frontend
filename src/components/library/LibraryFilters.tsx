8b28e537b07390bf0d5fe18fd32bd045503af319
bhardwajakshit <bhardwajakshit78@gmail.com>
2024-02-08 19:34:26 +0530

    Updated library headers

--- src/components/library/LibraryFilters.tsx
+++ src/components/library/LibraryFilters.tsx
@@ -1,18 +1,15 @@
+import { useNavigate } from "react-router-dom";
 import { PlusIcon } from "../../svgs/plusIcon";
 
 export const LibraryFilters = () => {
+  const navigate = useNavigate();
   return (
-    <div className="flex flex-row items-center gap-4 bg-white px-4 py-3 border border-gray-300 hover:border-gray-400 transition-colors duration-300 rounded-[3rem] shadow-lg">
+    <button
+      className="flex flex-row items-center gap-4 bg-white px-4 py-3 border border-gray-300 hover:border-gray-400 transition-colors duration-300 rounded-[3rem] shadow-lg text-lg text-gray-900 font-medium"
+      onClick={() => navigate("/create")}
+    >
       <PlusIcon />
-      <select
-        className="rounded-lg text-lg font-medium focus:outline-none focus:border-bluePrimary"
-        name="filters"
-        id="filters"
-      >
-        <option value="all">Add New</option>
-        <option value="recent">Recent</option>
-        <option value="most-viewed">Most Viewed</option>
-      </select>
-    </div>
+      Add New
+    </button>
   );
 };
--- src/components/library/LibraryQuickRevision.tsx
+++ src/components/library/LibraryQuickRevision.tsx
@@ -5,13 +5,15 @@ export const LibraryQuickRevision = () => {
     <div className="flex flex-row items-center gap-4 bg-white px-4 py-3 border border-gray-300 hover:border-gray-400 transition-colors duration-300 rounded-[3rem] shadow-lg">
       <ResetIcon />
       <select
-        className="rounded-lg text-lg font-medium focus:outline-none focus:border-bluePrimary w-44"
+        className="rounded-lg text-lg focus:outline-none focus:border-bluePrimary w-44 text-gray-900 font-medium"
         name="filters"
         id="filters"
       >
         <option value="all">Quick Revision</option>
-        <option value="recent">Recent</option>
-        <option value="most-viewed">Most Viewed</option>
+        <option value="today">Today</option>
+        <option value="last-week">Last Week</option>
+        <option value="last-15-days">Last 15 Days</option>
+        <option value="last-30-days">Last 30 Days</option>
       </select>
     </div>
   );
