46d449b572c094b3d061c967b3c57b252490ec9a
bhardwajakshit <bhardwajakshit78@gmail.com>
2024-02-13 17:57:33 +0530

    chore: updated auth button

--- src/components/auth/authButton.tsx
+++ src/components/auth/authButton.tsx
@@ -7,7 +7,7 @@ interface AuthButtonProps {
 const AuthButton = ({ label, handleAuth, disabled }: AuthButtonProps) => {
   return (
     <button
-      className="mt-4 px-12 py-2 rounded-lg text-lg font-semibold text-white hover:text-bluePrimary bg-bluePrimary hover:bg-transparent hover:border-gray-200 border border-transparent transition-colors duration-300"
+      className={`mt-4 px-12 py-2 rounded-lg text-lg font-semibold text-white  bg-bluePrimary hover:border-gray-200 border border-transparent transition-colors duration-300 ${disabled ? "opacity-50 cursor-not-allowed" : "hover:bg-transparent hover:text-bluePrimary"}`}
       disabled={disabled}
       onClick={handleAuth}
     >
